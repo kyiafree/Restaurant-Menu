8c8f569302d2fbb6c8b24ba6c44b3e20eca56cab
kyiafree <110514670+kyiafree@users.noreply.github.com>
2022-08-28 19:38:19 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+
+let menu = {
+    breakfast: ['avocado toast','french toast','creamUhWheat'],
+    lunch: ['shhhiiiChicken', "BolognaSamich"],
+    dinner: ['STEAK']
+}
+let drinks = {
+    soda: ['ginger ale','pepsi','coke'],
+    alochol: ['mimosas'],
+    hot:['coffee','tea'],
+    juice:['orange','cranberry','apple']
+} 
+let sides = {
+    fruits:['pineapples', 'strawberries','blueberries','mangos','apples'],
+    bread:['toast','crossiant']
+}
+
+
+
+
+
+const doSomething = ()=>{
+
+    let target = document.querySelector('.magi')
+
+    target.innerHTML = menu.breakfast[0]
+
+}
